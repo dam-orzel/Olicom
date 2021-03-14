e6e25f3baf4f99cba892f00922759a95afb7cc34
Damian Orzel <damian.orzel1995@gmail.com>
2021-03-14 21:13:13 +0100

    initializing project + axios + request + simple styling

---
+++ components/Table.vue
@@ -0,0 +1,60 @@
+<template>
+<div class="custom-table">
+  <table>
+      <thead>
+        <tr>
+          <th v-for="(item, id) in config" :key="id">
+            {{ item.name }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, id) in dataTable" :key="id">
+          <td v-for="(item, id) in config" :key="id">
+            {{ item.dataKey ? row[item.dataKey][item.propKey] : row[item.propKey]  }}
+          </td>
+        </tr>
+      </tbody>
+  </table>
+</div>
+</template>
+
+<script>
+export default {
+props: ["config", "dataTable"],
+}
+</script>
+
+<style lang="scss">
+.custom-table {
+  overflow-x: auto;
+  table {
+    width: 100%;
+    background: $navy;
+    padding: 15px 15px 45px 15px;
+    border-radius: 5px;
+    thead {
+      tr {
+        th {
+          text-align: left;
+          padding: 5px 10px 15px 10px;
+        }
+      }
+    }
+    tbody {
+      tr {
+        td {
+          border-bottom: 1px solid #343747;
+          padding: 10px 10px;
+        }
+        &:last-child {
+          td {
+            border-bottom: none;
+          }
+        }
+      }
+    }
+  }
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,67 @@
+<template>
+<section>
+  <header>
+    <h1>Olicom</h1>
+  </header>
+  <main class="container">
+    <Table :config="configList" :dataTable="usersList"/>
+  </main>
+</section>
+</template>
+
+<script>
+import Table from "@/components/Table.vue"
+
+export default {
+  components: {
+    Table
+  },
+  data() {
+    return {
+      usersList: null,
+      configList: [
+        {
+          propKey: "name",
+          name: "Imię i nazwisko",
+        },
+        {
+          propKey: `email`,
+          name: "E-mail",
+        },
+        {
+          dataKey: 'company',
+          propKey: `name`,
+          name: "Nazwa firmy",
+        },
+        {
+          dataKey: 'address',
+          propKey: "city",
+          name: "Miasto",
+        },
+        {
+          propKey: "website",
+          name: "Strona internetowa",
+        }
+      ]
+    }
+  },
+  async mounted() {
+    const users = await this.$axios.$get("https://jsonplaceholder.typicode.com/users")
+    this.usersList = users
+    console.log(users.detail)
+  }
+}
+</script>
+
+<style lang="scss">
+header {
+  background: $navy-light;
+  padding: 20px 0;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+  h1 {
+    color: $white;
+  }
+}
+</style>
